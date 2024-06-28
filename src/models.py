a4894cb185af22a28f9a6dc58504bcfc0c91eb1c
jairomoj <58726090+jairomoj@users.noreply.github.com>
2024-06-28 18:03:50 +0000

    First commit

--- src/models.py
+++ src/models.py
@@ -7,23 +7,96 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(100), nullable=False)
+    email = Column(String(50), nullable=False)
+    password = Column(String(12), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class Character(Base):
+    __tablename__ = 'character'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(100), nullable=False)
+    eye_color = Column(String(40), nullable=False)
+    hair_color = Column(String(40), nullable=False)
+    #person_id = Column(Integer, ForeignKey('person.id'))
+    #person = relationship(Person)
+
+    def to_dict(self):
+        return {}
+    
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    model = Column(String(40), nullable=False)
+    _class = Column(String(40), nullable=False)
+    #person_id = Column(Integer, ForeignKey('person.id'))
+    #person = relationship(Person)
+
+    def to_dict(self):
+        return {}
+    
+class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    population = Column(Integer, nullable=False)
+    climate = Column(String(40), nullable=False)
+    #person_id = Column(Integer, ForeignKey('person.id'))
+    #person = relationship(Person)
+
+    def to_dict(self):
+        return {}
+    
+class Fav_character(Base):
+    __tablename__ = 'fav_character'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    char_relation = Column(Integer, ForeignKey('character.id'), nullable=False)
+    user_relation = Column(Integer, ForeignKey('user.id'), nullable=False)
+
+    #person_id = Column(Integer, ForeignKey('person.id'))
+    #person = relationship(Person)
+
+    def to_dict(self):
+        return {}
+    
+class Fav_vehicle(Base):
+    __tablename__ = 'fav_vehicle'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    vehic_relation = Column(Integer, ForeignKey('vehicle.id'), nullable=False)
+    user_relation = Column(Integer, ForeignKey('user.id'), nullable=False)
+
+    #person_id = Column(Integer, ForeignKey('person.id'))
+    #person = relationship(Person)
+
+    def to_dict(self):
+        return {}
+    
+class Fav_planet(Base):
+    __tablename__ = 'fav_planet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    planet_relation = Column(Integer, ForeignKey('planet.id'), nullable=False)
+    user_relation = Column(Integer, ForeignKey('user.id'), nullable=False)
+
+    #person_id = Column(Integer, ForeignKey('person.id'))
+    #person = relationship(Person)
 
     def to_dict(self):
         return {}
